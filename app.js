3d0585ea5d9019d7a25d85b47733fb6c271fa1cb
Mohamad Khan <mohamadkhan19@gmail.com>
2017-02-26 04:41:32 -0800

    init commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var userRoutes = require('./routes/user');
 var formRoutes = require('./routes/form');
 
 var app = express();
-mongoose.connect('localhost:27017/restapi_test');
+mongoose.connect('mohamad:password123#@ds157819.mlab.com:57819/restapi_deployement');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
